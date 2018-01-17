f08e3126d65353e091f5eef8796c365f44102533
mjpark03 <mjpark03@gmail.com>
2018-01-18 08:40:10 +0900

    Add example 02 for item 02 in chapter 02

---
+++ chapter02/Item0202.java
@@ -0,0 +1,13 @@
+
+// JavaBeans Pattern - allows inconsistency, mandates mutability
+public class NutritionFacts {
+	// Parameters initialized to default values (if any)
+	private int servingSize = -1;
+	private int servings = -1;
+	private int calories = 0;
+	private int fat = 0;
+	private int sodium = 0;
+	private int carbohydrate = 0;
+
+	public NutritionFacts() { }
+}
