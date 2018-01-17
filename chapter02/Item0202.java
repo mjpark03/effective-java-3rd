37a47a709a88da8a5634c469f97f2af14f582492
mjpark03 <mjpark03@gmail.com>
2018-01-18 08:44:02 +0900

    Add setters to example 02 for item 02 in chapter 02

--- chapter02/Item0202.java
+++ chapter02/Item0202.java
@@ -10,4 +10,12 @@ public class NutritionFacts {
 	private int carbohydrate = 0;
 
 	public NutritionFacts() { }
+
+	// Setters
+	public void setServingSize(int val) { servingSize = val; }
+	public void setServings(int val) { servings = val; }
+	public void setCalories(int val) { calories = val; }
+	public void setFat(int val) { fat = val; }
+	public void setSodium(int val) { sodium = val; }
+	public void setCarbohydrate(int val) { carbohydrate = val; }
 }
