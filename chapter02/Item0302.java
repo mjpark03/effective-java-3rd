4248a4bf3743af828779af2bdffdab1681b0ed6b
mjpark03 <mjpark03@gmail.com>
2018-01-23 12:56:03 +0900

    Add example 02 for singleton implementation

---
+++ chapter02/Item0302.java
@@ -0,0 +1,8 @@
+
+public class Elvis {
+	private static final Elvis INSTANCE = new Elvis();
+	private Elvis(){ ... }
+	public static Elvis getInstance() { return INSTANCE; }
+
+	public void leaveTheBuilding() { ... }
+}
