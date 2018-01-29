bc6fb65c94333b8c067cfd76a8f09574ea7bde73
mjpark03 <mjpark03@gmail.com>
2018-01-30 08:41:28 +0900

    Add example 01 for creating noninstantiable class

---
+++ chapter02/Item0401.java
@@ -0,0 +1,9 @@
+
+// Noninstantiable utility class
+public class UtilityClass {
+
+	// Suppress default constructor for noninstantiability
+	private UtilityClass() {
+		throw new AssertionError();
+	}
+}
